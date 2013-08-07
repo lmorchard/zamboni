dc8359ccf20f2cc53e0c9631e17fc9a0fb99da9e
Les Orchard <me@lmorchard.com>
2013-08-07 14:23:29 -0400

    Tweaks to use MDN theme for API docs

--- docs/api/conf.py
+++ docs/api/conf.py
@@ -4,10 +4,13 @@ sys.path.append(os.path.abspath('../..'))
 from docs.conf import *  # noqa
 
 
-project = u'API v1'
+project = u'Firefox Marketplace API v1'
 version = release = '1.0'  # Should correspond to the API version number
 
-html_theme_path = ['../_themes']
+import mdn_theme
+html_theme_path = [mdn_theme.get_theme_dir()]
+html_theme = 'mdn'
+
 html_static_path = ['../_static']
 
 intersphinx_mapping = {}
